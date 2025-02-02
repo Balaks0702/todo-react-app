5f77ee4c99e8dc4aff1e6944a0cf63d5dbd59036
Balaks0702 <gayathriks0702@gmail.com>
2025-02-02 21:37:14 +0530

    ready to deploy

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,14 @@
 require("dotenv").config(); 
 const express = require("express"); //import
 const {connectToMongoDB} = require("./database"); //import database
+const path = require('path'); //import 
 const app = express(); //instance of express called app
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'build')));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, 'build/index.html'));
+});
 
 const router = require("./routes"); //import todos router
 app.use("/api", router); //prefix /api to endpoints
